ec9b53f61e5d98a61b9e69efa4dd3da201ee11e3
Emmanuel Bastien <os@ebastien.name>
2012-09-17 18:28:39 +0200

    Experiment with polymorphic decision tree.

--- src/Journey/DecisionTree.hs
+++ src/Journey/DecisionTree.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Journey.DecisionTree (
-      empty
-    , insert
+      insert
     , lookupWith
     , lookup
     , fromList
@@ -12,58 +15,68 @@ import qualified Data.Map as M
 import Data.Monoid (Monoid, mappend, mempty)
 import Prelude hiding (lookup)
 
+data Storable k v = forall a . StoreAttribute k a => MkStorable (Store a v)
+
+instance Show (Storable k v) where
+  show _ = "<Storable>"
+
+class StoreAttribute k a where
+  data Store a :: * -> *
+  store :: v -> k -> Store a v -> (v -> v) -> Storable k v
+  unstore :: v -> k -> Store a v -> v
+
+type StoredTree a = Storable a (Tree a)
+
+data RootTree a = RootTree [StoredTree a] (Tree a)
+
 -- | A decision tree.
-data Tree a b = Node (Tree a b) (M.Map b (Tree a b)) a
-              | Leaf a
-              | Empty
-                deriving (Show)
+data Tree a = Node (Tree a) (StoredTree a) a
+            | Leaf a
+            | Empty
+              deriving (Show)
 
 -- | A classification rule.
-type Rule b = [Maybe b]
+type Rule = [Bool]
 
 -- | Insert an item into a tree by following a classification rule.
-insert :: (Ord b, Monoid a) => Tree a b
-                            -> (Rule b, a)
-                            -> Tree a b
-insert tree (rule, item) = walk rule tree
-  where walk []     _ = Leaf item
-        walk (r:rs) t = case r of
-          Nothing -> case t of
-                       Empty      -> Node (walk rs Empty) M.empty item
-                       Node d m l -> let l' = item `mappend` l
-                                     in Node (walk rs d) m l'
-          Just c  -> case t of
-                       Empty      -> Node Empty (M.singleton c (walk rs Empty)) item
-                       Node d m l -> let t'  = M.findWithDefault Empty c m
-                                         t'' = walk rs t'
-                                         m'  = M.insert c t'' m
-                                         l'  = item `mappend` l
-                                     in Node d m' l'
+insert :: (Monoid a) => RootTree a
+                     -> (Rule, a)
+                     -> RootTree a
+insert (RootTree defs tree) (rule, item) = RootTree defs $ walk (zip rule defs) tree
+  where walk [] _ = Leaf item
+        walk ((active, MkStorable s):rs) t
+          | active = case t of
+              Empty                   -> let m' = store Empty item s (walk rs)
+                                         in Node Empty m' item
+              Node d (MkStorable m) l -> let m' = store Empty item m (walk rs)
+                                             l' = item `mappend` l
+                                         in Node d m' l'
+          | otherwise = case t of
+              Empty      -> Node (walk rs Empty) (MkStorable s) item
+              Node d m l -> let l' = item `mappend` l
+                            in Node (walk rs d) m l'
 
 -- | Lookup items from a tree by following a classification rule.
-lookupWith :: (Ord b, Monoid m) => (a -> m)
-                                -> (a -> Bool)
-                                -> Tree a b
-                                -> Rule b
-                                -> m
-lookupWith mmap pred tree rule = walk rule tree
-  where walk _      (Leaf i)              = mmap i
-        walk []     (Node d _ l) | pred l = walk [] d
-        walk (r:rs) (Node d m l) | pred l = case r of
-                                              Nothing -> left
-                                              Just c  -> mappend left $ right c
-          where left    = walk rs d
-                right c = walk rs $ M.findWithDefault Empty c m
+lookupWith :: (Monoid m) => (a -> m)
+                         -> (a -> Bool)
+                         -> RootTree a
+                         -> (Rule, a)
+                         -> m
+lookupWith mmap pred (RootTree _ tree) (rule, item) = walk rule tree
+  where walk _           (Leaf i)                           = mmap i
+        walk []          (Node d _ l)              | pred l = walk [] d
+        walk (active:rs) (Node d (MkStorable m) l) | pred l =
+          if active
+            then mappend left $ right
+            else left
+          where left  = walk rs d
+                right = walk rs $ unstore Empty item m
         walk _ _ = mempty
 
 -- | Lookup specialized to return the list of all matching items.
-lookup :: (Ord b) => Tree a b -> Rule b -> [a]
+lookup :: RootTree a -> (Rule, a) -> [a]
 lookup = lookupWith (:[]) $ const True
 
--- | The empty decision tree.
-empty :: Tree a b
-empty = Empty
-
 -- | Create a decision tree from a list of rules and items associations.
-fromList :: (Ord b, Monoid a) => [(Rule b, a)] -> Tree a b
-fromList = foldl insert Empty
+fromList :: (Monoid a) => [StoredTree a] -> [(Rule, a)] -> RootTree a
+fromList defaults = foldl insert $ RootTree defaults Empty
