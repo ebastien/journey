6c28921f023bac075abb8de3c43fcac8090d80b9
Emmanuel Bastien <os@ebastien.name>
2012-10-08 16:52:07 +0200

    Support for online qualified restrictions.

--- src/Journey/Restriction.hs
+++ src/Journey/Restriction.hs
@@ -1,8 +1,9 @@
 module Journey.Restriction (
     Restriction(..)
   , paxAllowed
-  , Cnx(..)
+  , Local(..)
   , Traffic(..)
+  , initiate
   , connect
   , complete
   , RestrictService(..)
@@ -46,30 +47,54 @@ serviceDenied = [ NoLocal, NoDisplay, TechnicalLanding ]
 paxAllowed :: RestrictService -> Bool
 paxAllowed r = not $ rPax r `elem` serviceDenied
 
-data Cnx c = Cnx c Bool Restriction
-             deriving (Eq, Show)
-data Traffic c = Traffic (Cnx c) (Cnx c) Qualifier
-                 deriving (Eq, Show)
+isQualified :: Restriction -> Bool
+isQualified r = r == QIntlOnlineCnxStop ||
+                r == QOnlineCnxStop ||
+                r == QOnlineCnx
 
-connect :: Eq c => Traffic c -> Traffic c -> Maybe (Traffic c)
-connect (Traffic x1 y1 (q1, p1))
-        (Traffic x2 y2 (q2, p2)) =
+data Local c = Local { lCarrier :: c
+                     , lIntl :: Bool
+                     , lRestriction :: Restriction
+                     , lBoardQual :: Bool
+                     , lOffQual :: Bool
+                     } deriving (Eq, Show)
+
+data Cnx c = Cnx { cCarrier :: c
+                 , cIntl :: Bool
+                 , cRestriction :: Restriction
+                 } deriving (Eq, Show)
+
+data Traffic c = Traffic { tInCnx :: Cnx c
+                         , tInQual :: Bool
+                         , tOutCnx :: Cnx c
+                         , tOutQual :: Bool
+                         , tQFlag :: Bool
+                         } deriving (Eq, Show)
+
+initiate :: Local c -> Maybe (Traffic c)
+initiate (Local c t r q p) = Just $ Traffic cnx q cnx p f
+  where cnx = Cnx c t r
+        f = isQualified r && (p || not q)
+
+connect :: Eq c => Local c -> Traffic c -> Maybe (Traffic c)
+connect (Local c2 t2 r2 q2 p2)
+        (Traffic x1 q1 y1 p1 f) =
   if (not p1 || p1 && l1) &&
      (not q2 || q2 && l2)
-    then Just $ Traffic x1 y2 q
+    then Just $ Traffic x1 q (Cnx c2 t2 r2) p f'
     else Nothing
-  where q = ( q1 || not (p1 || l1)
-            , p2 || not (q2 || l2) )
-        (Cnx a1 t1 r1) = y1
-        (Cnx a2 t2 r2) = y2
-        o = a1 == a2
+  where q = q1 || not (p1 || l1)
+        p = p2 || not (q2 || l2)
+        (Cnx c1 t1 r1) = y1
+        o = c1 == c2
         l1 = isCnxAllowed r1 o t2
         l2 = isCnxAllowed r2 o t1
+        f' = f && isQualified r2 && (q2 || not p2)
 
 complete :: Traffic c -> Maybe (Traffic c)
-complete t@(Traffic _ _ (q, p)) = if not (q || p)
-                                    then Just t
-                                    else Nothing
+complete t = if not (tInQual t || tOutQual t || tQFlag t)
+               then Just t
+               else Nothing
 
 isCnxAllowed :: Restriction -> Bool -> Bool -> Bool
 isCnxAllowed r o t = case r of
--- testsuite/src/Journey/Tests/Restriction.hs
+++ testsuite/src/Journey/Tests/Restriction.hs
@@ -1,28 +1,28 @@
 module Journey.Tests.Restriction (tests) where
 
-import Data.Maybe (fromJust)
+import Data.Maybe (isJust)
 
 import Test.HUnit
 import Test.Framework.Providers.HUnit (testCase)
 
 import Journey.Restriction
 
+l1 = Local 1 False NoRestriction False False
+l2 = Local 1 False NoLocal False False
+
+l3 = Local 1 False QOnlineCnx False True
+l4 = Local 1 False QOnlineCnx False False
+l5 = Local 1 False QOnlineCnx True False
+
+assertAllowed c = isJust c @?= True
+assertDenied c = isJust c @?= False
+
 tests = [
-    test1
-  , test2
+    testCase "NoRestriction" . assertAllowed $ initiate l1 >>= connect l1 >>= complete
+  , testCase "NoLocal"       . assertDenied  $ initiate l1 >>= connect l2 >>= complete
+  , testCase "QOnlineCnx_09" . assertDenied  $ initiate l3 >>= connect l5 >>= complete
+  , testCase "QOnlineCnx_10" . assertDenied  $ initiate l3 >>= connect l4 >>= complete
+  , testCase "QOnlineCnx_11" . assertDenied  $ initiate l4 >>= connect l4 >>= complete
+  , testCase "QOnlineCnx_12" . assertAllowed $ initiate l3 >>= connect l3 >>= connect l5 >>= complete
+  , testCase "QOnlineCnx_13" . assertDenied  $ initiate l3 >>= connect l4 >>= connect l5 >>= complete
   ]
-
-t1 = Traffic (Cnx 1 False NoRestriction)
-             (Cnx 1 False NoRestriction)
-             (False, False)
-
-t2 = Traffic (Cnx 1 False NoRestriction)
-             (Cnx 1 False NoLocal)
-             (False, False)
-
-c11 = connect t1 =<< Just t1
-c12 = connect t1 =<< Just t2
-
-test1 = testCase "Simple" $ c11 @?= Just t1
-test2 = testCase "NoLocal" $ (complete =<< c12) @?= Nothing
-
