113f304547706ad0dcee9d7694dcac741430420a
Emmanuel Bastien <os@ebastien.name>
2012-10-16 09:01:24 +0200

    Replace parallel strategies with parallel IO.

--- testsuite/src/Benchs.hs
+++ testsuite/src/Benchs.hs
@@ -8,8 +8,9 @@ import Data.Foldable (foldMap)
 import qualified Data.Text.Lazy.IO as T
 import Data.Text.Lazy.Builder (toLazyText, Builder)
 import System.Environment (getArgs)
-import Control.Parallel.Strategies (withStrategy, parList, rseq)
 import Text.Printf (printf)
+import Control.Concurrent (getNumCapabilities)
+import Control.Concurrent.ParallelIO.Local (withPool, parallel_)
 
 import Journey.Ssim (readSsimFile, ssimRegularSegments)
 import Journey.MCT.OAGParser (readMCTFile)
@@ -24,8 +25,8 @@ import Journey.Parsers (toPort)
 buildInFile :: FilePath -> Builder -> IO ()
 buildInFile f = T.writeFile f . toLazyText
 
-parallelBuild :: [Builder] -> IO ()
-parallelBuild bs = sequence_ $ withStrategy (parList rseq) chunks
+parallelBuild :: Int -> [Builder] -> IO ()
+parallelBuild n bs = withPool n $ \p -> parallel_ p chunks
   where chunks = uncurry buildInFile <$> zip files bs
         files = printf "part_%05d" <$> [0::Int ..]
 
@@ -43,4 +44,6 @@ main = do
       regn = pruneLookup mctdb
       bldr = buildPathPeriod segs geos regn
 
-  parallelBuild $ buildSplit covs bldr 10000
+  thds <- getNumCapabilities
+
+  parallelBuild thds $ buildSplit covs bldr 10000
