c7982e706c739705dc9da4e4831a72298511fc56
Emmanuel Bastien <os@ebastien.name>
2012-11-16 10:37:21 +0100

    Binary for itineraries on a list of OnDs.

---
+++ src/OnDItineraries.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Data.Functor ((<$>))
+import Data.Maybe (fromJust)
+import Data.Foldable (foldMap)
+import qualified Data.Text.Lazy.IO as LT
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Encoding as T
+import Data.Text.Lazy.Builder (toLazyText)
+import System.Environment (getArgs)
+
+import Journey.Ssim (readSsimFile, ssimRegularSegments)
+import Journey.MCT.OAGParser (readMCTFile)
+import Journey.MCT.Attributes (attributes)
+import Journey.MCT.Tree (fromList, pruneLookup)
+import Journey.Route (coverages)
+import Journey.GeoCoord (loadReferences, assocToCities, adjacency, portToCountry)
+import Journey.Builder (buildForOnD, buildPathPeriod)
+import Journey.OnDSegments (fromSegments, toOnDs, fromOnD)
+import Journey.Types (OnD)
+import Journey.Parsers (toPort)
+
+readOnDs :: String -> IO [OnD]
+readOnDs f = return . map parse . T.lines =<< T.readFile f
+  where parse row = let (a:b:[]) = T.split (==',') row in (port a, port b)
+        port = fromJust . toPort . T.encodeUtf8
+
+main :: IO ()
+main = do
+  [refsFile, mctFile, ssimFile, ondsFile] <- getArgs
+  refs <- loadReferences refsFile
+  mctdb <- fromList attributes <$> readMCTFile mctFile
+  segdb <- fromSegments . assocToCities refs . ssimRegularSegments
+       <$> readSsimFile ssimFile
+  onds <- readOnDs ondsFile
+
+  let covs = take 3 . coverages . adjacency refs $ toOnDs segdb
+      segs = fromOnD segdb
+      geos = portToCountry refs
+      regn = pruneLookup mctdb
+      bldr = buildForOnD covs $ buildPathPeriod segs geos regn
+
+  LT.putStr . toLazyText $ foldMap bldr onds
